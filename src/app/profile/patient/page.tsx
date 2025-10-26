7b10a9bb0c5f0cf83f01d9ab28c3a452aaa4c678
unknown <maha.ahmad.k@hotmail.com>
2025-10-26 15:20:23 +0300

    fix the deploy error

--- src/app/profile/patient/page.tsx
+++ src/app/profile/patient/page.tsx
@@ -1,14 +1,12 @@
-// import { PatientSuccess } from "@/features/dashboard/ui/PatientForm";
 
-// import React from 'react'
-
+import React from 'react'
 
 
-// export const page = () => {
-//   return (
-//     <div>
 
-//       <PatientSuccess patientData={formData} />
-//     </div>
-//   )
-// }
+export default function Page() {
+  return (
+    <div>
+        صفحة ملف المريض
+    </div>
+  )
+}

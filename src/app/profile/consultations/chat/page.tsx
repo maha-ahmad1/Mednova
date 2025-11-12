4c6a132c971d60af88c4a16c663702f54676696b
maha-ahmad1 <maha.ahmad.k@hotmail.com>
2025-11-12 13:10:40 +0300

    delete the chat file

--- src/app/profile/consultations/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-// import ChatView from '@/features/consultations/Chat/ui/Chat'
-// export default function page() {
-//   return (
-//     <div>
-//         <ChatView />
-//     </div>
-//   )
-// }
